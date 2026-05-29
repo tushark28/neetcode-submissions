52914ad8df8249d0d6dcf67d9beaa2a003a85041
Tushar Kamboj <iamtktushar@gmail.com>
2026-05-29 16:19:38 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+   public:
+    string minWindow(string s, string t) {
+        int l = 0, have = 0, len = INT_MIN;
+        string ans = "";
+        unordered_map<char, int> mp, window;
+
+        for (auto& c : t) {
+            mp[c]++;
+        }
+
+        for (int r = 0; r < s.size(); r++) {
+            window[s[r]]++;
+
+            if (mp.count(s[r]) && mp[s[r]] == window[s[r]]){
+                have++;
+            }
+
+            while(have == mp.size()){
+                if (len == INT_MIN || len > r-l+1){
+                    len = r-l+1;
+                    ans = s.substr(l, len);
+                }
+                window[s[l]]--;
+                if(mp.count(s[l]) && mp[s[l]] > window[s[l]]){
+                    have--;
+                }
+                l++;
+            }
+        }
+
+        return ans;
+    }
+};
